4649302f0532e4713ea879c94883911fe3c211ef
DariusJderu1 <jderudarius@gmail.com>
2026-04-29 21:04:19 +0300

    Add HomePage component

---
+++ src/components/app/HomePage.jsx
@@ -0,0 +1,28 @@
+// import { Link } from "react-router-dom";
+
+function HomePage() {
+
+    return (
+
+        <main>
+            <p>Curated clothing for everyday style</p>
+
+             <h1>
+                Build a wardrobe
+                <br />
+                you actually wear.
+            </h1>
+
+            <p>
+                Discover a handpicked collection of clothing made for simple outfits,
+                clean looks, and pieces you can wear again and again.
+            </p>
+
+            {/* <Link to="/shop">
+                Explore the Collection
+            </Link> */}
+        </main>
+    );
+}
+
+export default HomePage;
