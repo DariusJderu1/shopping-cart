69c710bb39a8f536d1f9881b1977f8d21d2d8cfd
DariusJderu1 <jderudarius@gmail.com>
2026-04-29 20:48:44 +0300

    Add router version of the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/App.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+import routes from "./routes.jsx";
+
 import "./styles/index.css";
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>,
 );
