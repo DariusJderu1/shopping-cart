ba5f7df652fc0876eebe2c8dab31f413d77ceee0
DariusJderu1 <jderudarius@gmail.com>
2026-05-06 13:20:17 +0300

    Add product details loading and error state management

--- src/components/app/ProductDetails.jsx
+++ src/components/app/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams, useOutletContext } from "react-router";
 import { FaShoppingCart } from "react-icons/fa";
+import CircularProgress from "@mui/material/CircularProgress";
 import QuantitySelector from "../ui/QuantitySelector";
 // import styles from "../../styles/app/ProductDetails.module.css";
 
@@ -9,6 +10,28 @@ function ProductDetails() {
     const { productId } = useParams();
 
     const product = shopData.clothesList.find(item => item.id === parseInt(productId));
+
+    //======================================================
+
+    if(shopData.loading) {
+
+        return (
+
+            <main>
+                <CircularProgress aria-label="Loading…" />
+            </main>
+        );
+    }
+
+    if(!shopData.loading && shopData.error) {
+    
+        return (
+
+            <main>
+                <h1>{shopData.error}</h1>
+            </main>
+        );
+    }
 
     return (
 
